4a374af0e6f57ee27c3b0bb86c2f45ec4e70e6c7
Carlos Alexandre <107065086+alexandrecarloss@users.noreply.github.com>
2023-08-23 19:46:12 -0300

    Update Leitura_Escrita_fscanf_fprintf.cpp

--- ED/exercicios/Aula2_Arquivos/Leitura_Escrita_fscanf_fprintf.cpp
+++ ED/exercicios/Aula2_Arquivos/Leitura_Escrita_fscanf_fprintf.cpp
@@ -8,15 +8,22 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-    //Criando uma vari·vel ponteiro para o arquivo
+    //Criando uma vari√°vel ponteiro para o arquivo
     FILE *pont_arq;
-    char nome[100], n[100];
-    int idade, i;
+    char nome[100], n[100], espaco;
+    int idade, i, tamString;
     float altura, a;
     pont_arq = fopen("teste.txt", "a");
     if(pont_arq) {
         printf("Digite nome, idade e altura: ");
         scanf("%100[^\n]%d%f", nome, &idade, &altura);
+
+        tamString = strlen(nome);
+        for(i = 0; i< tamString; i++) {
+            if(nome[i] == ' ') {
+                nome[i] = '_';
+            }
+        }
         fprintf(pont_arq, "%s \n", nome);
         fprintf(pont_arq, "%d \n", idade);
         fprintf(pont_arq, "%.2f \n", altura);
@@ -30,6 +37,12 @@ int main() {
     if(pont_arq){
         printf("\tDados lidos do arquivo:\n");
         while(fscanf(pont_arq, "%s%d%f", nome, &idade, &altura) != EOF) {
+            for(i = -1; i< tamString; i++) {
+                if(nome[i] == '_') {
+
+                    nome[i] = ' ';
+                }
+            }
             cout << "Registro: " << cont;
             printf("\n\tNome: %s\n\tIdade: %d\n\tAltura: %.2f\n", nome, idade, altura);
             cont++;
@@ -39,33 +52,6 @@ int main() {
     else
         printf("\nERRO ao abrir arquivo!\n");
 
-
-    int opcao = 0;
-    cont = -1;
-    pont_arq = fopen("teste.txt", "r+");
-    cout << "qual o registro quer alterar? \n";
-    scanf("%d", &opcao);
-    if(pont_arq){
-        while(fscanf(pont_arq, "%s%d%f", nome, &idade, &altura) != EOF) {
-            if(cont == opcao) {
-                printf("Digite nome, idade e altura: ");
-                scanf("%100[^\n]%d%f", n, &i, &a);
-                fprintf(pont_arq, "%s - %s\n", nome, "as");
-                strcpy(n, nome);
-                //fprintf(pont_arq, "%d \n", idade);
-                //fprintf(pont_arq, "%.2f \n", altura);
-                /*
-                fprintf(pont_arq, "%s \n", nome);
-                fprintf(pont_arq, "%d \n", idade);
-                fprintf(pont_arq, "%.2f \n", altura);
-                */
-            }
-            cont++;
-        }
-        fclose(pont_arq);
-    }
-    else
-        printf("\nERRO ao abrir arquivo!\n");
 
 
 
